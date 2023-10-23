e1c948af6e941a38f0f78894ca9e05454a91d438
JimmyCauja <jimmycauja@gmail.com>
2023-10-23 15:24:13 -0500

    se creeo un circulo que sea como un espejo del otro

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let noiseTime = 0;
-let noiseTam = 10;
+let noiseTam = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,11 +14,19 @@ function draw() {
 
   let tam = map(noise(noiseTime), 0, 1, 50, 5);
   noiseTime += 0.006;
-  noiseTam += 0.1;
+  noiseTam += 0.01;
 
-  fill(255);
-  stroke(0, 50);
-  strokeWeight(4);
+  fill(180, 100, 13);
+  stroke(100, 50, 0, 50);
+  strokeWeight(5);
   circle(posX, posY, tam);
 
+  let posXRef = posX;
+  let posYRef = windowHeight - posY;
+  let tamRef = tam;
+
+  fill(100, 50, 130);
+  stroke(100, 150, 10, 50);
+  strokeWeight(5);
+  circle(posXRef, posYRef, tamRef);
 }
