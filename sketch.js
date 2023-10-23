699bd1b5671569dd282b59b0644a2f0ef7275792
JimmyCauja <jimmycauja@gmail.com>
2023-10-23 14:55:03 -0500

    Se creo un circulo con perlin noise con diferentes tamaños

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let noiseTime = 0;
+let noiseTam = 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+
+  // background(255);
+
+  let posX = map(noise(noiseTime), 0, 1, 0, windowWidth);
+  let posY = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+
+  let tam = map(noise(noiseTime), 0, 1, 50, 5);
+  noiseTime += 0.006;
+  noiseTam += 0.1;
+
+  fill(255);
+  stroke(0, 50);
+  strokeWeight(4);
+  circle(posX, posY, tam);
+
 }
