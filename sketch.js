715b476c195422259d68cca6289e211cf53aff9a
JimmyCauja <jimmycauja@gmail.com>
2023-10-26 00:40:58 -0500

    Se creo lineas con Perlin y Map y se cambio la cromatica

--- sketch.js
+++ sketch.js
@@ -1,31 +1,33 @@
 let noiseTime = 0;
-let noiseTam = 20;
+let noiseTam = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(25);
+  background(10, 35, 70);
+  smooth(50);
 }
 
 function draw() {
 
-  let posX = map(noise(noiseTime), 0, 1, 0, windowWidth);
-  let posY = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+  let posX1 = map(noise(noiseTime), 0, 1, 0, windowWidth);
+  let posY1 = map(noise(noiseTam), 0, 1, 0, windowHeight);
+  let posX2 = map(noise(noiseTime + 0.1), 0, 1, 0, windowWidth);
+  let posY2 = map(noise(noiseTam + 0.1), 0, 1, 0, windowHeight);
 
   let tam = map(noise(noiseTime), 0, 1, 50, 5);
-  noiseTime += 0.006;
-  noiseTam += 0.01;
+  noiseTime += 0.004;
+  noiseTam += 0.011;
 
-  fill(214, 204, 153);
-  stroke(100, 50, 0, 50);
-  strokeWeight(5);
-  circle(posX, posY, tam);
+  stroke(179, 70, 90, 60);
+  strokeWeight(2);
+  line(posX1, posY1, posX2, posY2);
 
-  let posXRef = posX;
-  let posYRef = windowHeight - posY;
-  let tamRef = tam;
+  let reflY1 = windowHeight - posY1;
+  let reflY2 = windowHeight - posY2;
+  let tamrefl = tam;
 
-  fill(0, 21, 36);
-  stroke(214, 204, 10, 50);
-  strokeWeight(5);
-  circle(posXRef, posYRef, tamRef);
+  stroke(243, 159, 90, 30);
+  strokeWeight(2);
+  line(posX1, reflY1, posX2, reflY2, tamrefl);
+  
 }
